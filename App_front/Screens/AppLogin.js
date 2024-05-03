31e2a0a74614f193ff05854ac9c8aef5a99302d8
ChanHyuckYou <qqs4562@naver.com>
2024-05-03 18:41:26 +0900

    Update ChanHyuck: UI개선 (반응형 디자인 요소)

--- App_front/Screens/AppLogin.js
+++ App_front/Screens/AppLogin.js
@@ -5,11 +5,15 @@ import { Color, FontSize, FontFamily } from "../GlobalStyles";
 import { useNavigation } from '@react-navigation/native';
 import {useState} from "react";
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {heightPercentage, leftPercentage, topPercentage, widthPercentage} from "./Window";
 
 const AppLogin = () => {
     const navigation = useNavigation();
     const [userEmail, setuserEmail] = useState("");
     const [userPwd, setuserPwd] = useState("");
+    // 로그인 성공 후
+
 
     function login() {
         if (userEmail.trim() === "") {
@@ -20,7 +24,7 @@ const AppLogin = () => {
             // admin과 1234로 바로 로그인 처리
             if (userEmail === "admin" && userPwd === "1234") {
                 console.log("관리자로 로그인 성공");
-                navigation.navigate('UserPage'); // 성공 시 이동할 페이지
+                navigation.navigate('UserPage', { userEmail: userEmail });
             } else {
                 axios.post("http://43.201.92.62/user/login", {
                     userid: userEmail,
@@ -31,7 +35,9 @@ const AppLogin = () => {
 
                         if (resp.data !== null && resp.data !== "") {
                             console.log("로그인 성공");
-                            navigation.navigate('UserPage');
+                            // 로그인 성공 후
+                            // AsyncStorage.setItem('userEmail', userEmail);
+                            navigation.navigate('UserPage', { userEmail: userEmail });
                         } else {
                             Alert.alert("로그인 실패", "아이디나 비밀번호를 확인하세요.");
                             setuserEmail("");
@@ -46,33 +52,6 @@ const AppLogin = () => {
         }
     }
 
-
-
-    // function login() {
-    //     if (userEmail.trim() === "") {
-    //         Alert.alert("아이디 입력 확인", "아이디가 입력되지 않았습니다.");
-    //     } else if (userPwd.trim() === "") {
-    //         Alert.alert("비밀번호 입력 확인", "비밀번호가 입력되지 않았습니다.");
-    //     } else {
-    //         axios.post("http://43.201.92.62/user/login",
-    //             null,
-    //             {params: {userid: userEmail, password: userPwd}}
-    //         ).then(function (resp) {
-    //             console.log(resp.data);
-    //
-    //             if (resp.data !== null && resp.data !== "") {
-    //                 console.log("로그인 성공");
-    //                 navigation.navigate('UserPage')
-    //             } else {
-    //                 Alert.alert("로그인 실패", "아이디나 비밀번호를 확인하세요.");
-    //                 setEmail("");
-    //                 setPassword("");
-    //             }
-    //         }).catch(function (err) {
-    //             console.log(`Error Message: ${err}`);
-    //         })
-    //     }
-    // }
 
 
     return (
@@ -125,32 +104,33 @@ const AppLogin = () => {
 
 const styles = StyleSheet.create({
     viewLayout: {
-        height: 36,
-        width: 267,
+        width: widthPercentage(267),
+        height: heightPercentage(36),
         position: "absolute",
     },
     childPosition: {
-        left: 0,
-        top: 0,
+        top: topPercentage(0), // 기존에 고정값으로 사용되던 top: 50 대신
+        left: leftPercentage(0),
     },
     textTypo: {
-        height: 23,
+        height: heightPercentage(23),
+        width: widthPercentage(400),
         textAlign: "left",
         color: Color.colorWhite,
         fontSize: FontSize.size_xl,
-        top: 8,
+        top: topPercentage(5),
         fontFamily: FontFamily.interSemiBold,
         fontWeight: "600",
         position: "absolute",
     },
     passwordLayout: {
-        height: 41,
-        width: 267,
+        width: widthPercentage(267),
+        height: heightPercentage(41),
         position: "absolute",
     },
     text2Typo: {
-        height: 18,
-        width: 248,
+        width: widthPercentage(268),
+        height: heightPercentage(18),
         fontSize: FontSize.size_mini,
         alignSelf: "center",
         color: Color.colorBlack,
@@ -167,91 +147,92 @@ const styles = StyleSheet.create({
     },
     child: {
         backgroundColor: Color.colorOrangered,
-        height: 36,
-        width: 267,
+        width: widthPercentage(267),
+        height: heightPercentage(36),
         position: "absolute",
     },
     text: {
-        left: 105,
-        width: 56,
+        left: leftPercentage(105),
+        width: widthPercentage(56),
     },
     view1: {
-        top: 425,
+
+        top: topPercentage(425),
         alignSelf: "center",
     },
     text1: {
-        left: 96,
-        width: 74,
+        left: leftPercentage(96),
+        width: widthPercentage(74),
+
     },
     view2: {
-        top: 605,
+        top: topPercentage(605),
         alignSelf: "center",
     },
     id: {
-        left: 65,
-        width: 147,
+        left: leftPercentage(65),
+        width: widthPercentage(147),
+
     },
     view3: {
-        top: 507,
+        top: topPercentage(507),
         alignSelf: "center",
     },
     passwordChild: {
         borderStyle: "solid",
         borderColor: Color.colorOrangered,
         borderWidth: 3,
-        left: 0,
-        top: 0,
+        top: topPercentage(0), // 기존에 고정값으로 사용되던 top: 50 대신
+        left: leftPercentage(0),
         backgroundColor: Color.colorWhite,
     },
     password1: {
-        top: 10,
-        left: 13,
+        top: topPercentage(10), // 기존에 고정값으로 사용되던 top: 50 대신
+        left: leftPercentage(13),
         color: Color.colorBlack,
-        width: 245,
         fontFamily: FontFamily.interLight,
         fontStyle: "italic",
-        height: 23,
+        width: widthPercentage(245),
+        height: heightPercentage(23),
         textAlign: "left",
         fontSize: FontSize.size_xl,
         position: "absolute",
     },
     password: {
-        top: 311,
+        top: topPercentage(311), // 기존에 고정값으로 사용되던 top: 50 대신
         alignSelf: "center",
 
     },
     id1: {
-        top: 239,
+        top: topPercentage(239), // 기존에 고정값으로 사용되던 top: 50 대신
         alignSelf: "center",
     },
     appNupan: {
-        top: 630,
+        top: topPercentage(571),
     },
     text2: {
-        top: 530,
+        top: topPercentage(479),
     },
     appNupan1: {
-        top: 35,
+        top: topPercentage(35),
         alignSelf: "center",
         fontSize: FontSize.size_5xl,
-        width: 150,
-        height: 29,
+        width: widthPercentage(136),
+        height: heightPercentage(29),
         fontFamily: FontFamily.interSemiBold,
         fontWeight: "600",
         color: Color.colorBlack,
     },
     id3: {
-        top: 200,
+        top: topPercentage(200),
         fontSize: FontSize.size_mid,
-        width: 130,
-        height: 21,
+        width: widthPercentage(130),
+        height: heightPercentage(21),
         fontFamily: FontFamily.interLight,
-        left: 60,
+        left: leftPercentage(60),
     },
     view: {
         flex: 1,
-        width: "100%",
-        height: 800,
         overflow: "hidden",
         backgroundColor: Color.colorWhite,
     },
